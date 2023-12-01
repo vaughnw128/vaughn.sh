e37dec78399940a6da66f680bb8cc6d40c095dda
vaughnw128 <vww9966@rit.edu>
2023-11-30 22:50:35 -0500

    Working left nav on specific pages, sticks to scroll

--- components/LeftNav.vue
+++ components/LeftNav.vue
@@ -1,17 +1,12 @@
 <script setup>
-const route = useRoute()
-//const currentRoute = route.path
-const currentRoute = route.path.replace(/^\/|\/$/g, '');
-const folderContent = await queryContent(currentRoute).find()
+const currentCategory = useRoute().params["slug"][0]
+const folderContent = await queryContent(currentCategory).find()
 </script>
 
 <template>
     <div>
       <div>
         <div class="text-gray-700 dark:text-gray-200">
-            <pre>
-                {{ currentRoute }}
-            </pre>
             <ContentNavigation>
                 <ul>
                     <NuxtLink
@@ -24,27 +19,6 @@ const folderContent = await queryContent(currentRoute).find()
                     </NuxtLink>
                 </ul>
             </ContentNavigation>
-            <!-- <ContentNavigation>
-            <ul>
-                <ContentNavig path="/recipes" v-slot="{ list }">
-                <NuxtLInk v-for="doc in list" :key="doc._path" :to="doc._path">
-                <li> {{ doc.navTitle || doc.title}} </li>
-                </NuxtLink>
-                </ContentList>
-            </ul>
-            </ContentNavigation> -->
-                <!-- <nav>
-                    <ContentNavigation v-slot="{ navigation }" :query="articles">
-                    <ul>
-                        <li v-for="link of navigation" :key="link._path">
-                            <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
-                        </li>
-                    </ul>
-                    </ContentNavigation>
-                </nav> -->
-                <!-- <pre>
-                    {{ articles }}
-                </pre> -->
         </div>
       </div>
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,23 @@
+<script setup>
+const currentCategory = useRoute().params["slug"][0]
+const navPaths = ['recipes', 'projects', 'security']
+</script>
+
 <template>
-    <LeftNav class="flex h-screen max-w-[10%] px-2 py-4 m-auto bg-white sm:px-4 sm:shadow dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg" />
-    <div class="max-w-[40%] px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg">
+  <div class="flex justify-center space-x-4">
+    <div v-if="navPaths.includes(currentCategory)" class="sticky top-5 h-full w-[10%] px-2 py-4 bg-white sm:px-4 sm:shadow dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg">
+      <LeftNav />
+    </div>
+
+    <div class="w-[40%] px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg">
       <main class="max-w-none prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-900 hover:prose-a:text-primary-400 prose-a:font-normal prose-a:no-underline prose-a:border-dashed prose-a:border-b hover:prose-a:border-solid hover:prose-a:border-primary-400">
         <slot>
-          <h1 class="text-2xl">
-            Page not found
-          </h1>
-        </slot>
-      </main> 
+            <h1 class="text-2xl">
+              Page not found
+            </h1>
+          </slot>
+        </main>
     </div>
+    <div v-if="navPaths.includes(currentCategory)" class="w-[10%]" />
+  </div>
 </template>
