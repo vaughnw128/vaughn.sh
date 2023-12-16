97e6f12304bc93e43f26b565036586117e0a5b91
vaughnw128 <vww9966@rit.edu>
2023-12-16 01:08:34 -0500

    spacing update

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@ const navPaths = ['recipes', 'projects', 'security']
 
 </script>
 
-<template>
+<!-- <template>
   <div class="grid grid-cols-3 justify-center gap-4">
-    <div class="items-center ms-auto flex flex-col space-y-2">  
+    <div class="max-w-2xl items-center ms-auto flex flex-col space-y-2">  
           <div class="py-sm animated-text-gradient font-adelia text-3xl py-2">
             VAUGHN'S ZONE
           </div>
@@ -17,7 +17,7 @@ const navPaths = ['recipes', 'projects', 'security']
 
     <div>
       <Navbar />
-      <div class="bg-slate-800 px-8 py-10 m-auto ring-1 ring-gray-700 rounded-lg">
+      <div class="max-w-2xl bg-slate-800 px-8 py-10 m-auto ring-1 ring-gray-700 rounded-lg">
         <main class="max-w-none prose prose-invert prose-pre:bg-gray-900 hover:prose-a:text-primary-400 prose-a:font-normal prose-a:decoration-dotted hover:prose-a:decoration-wavy hover:prose-a:border-primary-400">
           <slot>
               <h1 class="text-2xl">
@@ -28,5 +28,33 @@ const navPaths = ['recipes', 'projects', 'security']
       </div>
     </div>
 
+  </div>
+</template> -->
+
+<template>
+  <div class="flex justify-center space-x-2">
+    <div class="flex flex-col w-[25%] px-4 py-4 sm:px-2 pt-2">
+        <div class="text-right animated-text-gradient font-adelia text-3xl py-2">
+        VAUGHN'S ZONE
+        </div>
+        <div class="flex flex-row-reverse">
+          <div v-if="navPaths.includes(currentCategory)" class="w-[90%] bg-gray px-4 py-4 bg-slate-800 ring-1 ring-gray-700 rounded-lg">
+            <LeftNav />
+          </div>
+      </div>
+    </div>
+    <div class="w-[50%]">
+      <Navbar />
+      <div class="bg-slate-800 px-8 py-10 m-auto ring-1 ring-gray-700 rounded-lg">
+        <main class="prose prose-invert prose-pre:bg-gray-900 hover:prose-a:text-primary-400 prose-a:font-normal prose-a:decoration-dotted hover:prose-a:decoration-wavy hover:prose-a:border-primary-400">
+          <slot>
+              <h1 class="text-2xl">
+                Page not found
+              </h1>
+            </slot>
+          </main>
+      </div>
+    </div>
+    <div class="w-[25%]" />
   </div>
 </template>
